e5d836caac074891eee5352cc9e747f349da4a8a
Kel Cecil <kelcecil@praisechaos.com>
2010-12-13 23:23:28 -0500

    added iteration through the tweets and filtering those tweets that are not from Morgantown or WV

--- prtstat.py
+++ prtstat.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# Do it for the cross-platform baby...
 
 # For performing the GET request
 import urllib2;
@@ -13,4 +14,11 @@ data = response.read()
 # Parse the JSON data and print it to the terminal
 # That's all for now...
 nativeData = json.loads(data)
-print nativeData
+
+for tweet in nativeData[u'results']:
+    # Problem: Some people that are far, far away from Morgantown use :pRT frequently in their tweets for a reason normal humans can not comprehend.
+    # Solution:  Ensure that the locations of tweets are in Morgantown or West Virginia.  This change filtered only the annoying :pRT messages and left the sensible messages alone.
+    if "West Virginia" in tweet[u'location'] or "Morgantown" in tweet[u'location']:
+        print tweet[u'text'];
+    else:
+        continue
