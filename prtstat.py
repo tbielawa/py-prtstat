9f130249d462074b3f55635c227e320fc065290a
Kel Cecil <kelcecil@praisechaos.com>
2010-12-13 23:30:01 -0500

    made some changes to facilitate experiments in the form of adding functions to the script

--- prtstat.py
+++ prtstat.py
@@ -7,18 +7,24 @@ import urllib2;
 # For parsing the JSON result
 import json;
 
-# Perform the search GET
-response = urllib2.urlopen('http://search.twitter.com/search.json?q=geocode:39.633611,-79.950556,25mi%20PRT')
-data = response.read()
+def main():
 
-# Parse the JSON data and print it to the terminal
-# That's all for now...
-nativeData = json.loads(data)
+    # Perform the search GET
+    response = urllib2.urlopen('http://search.twitter.com/search.json?q=geocode:39.633611,-79.950556,25mi%20PRT')
+    data = response.read()
 
-for tweet in nativeData[u'results']:
-    # Problem: Some people that are far, far away from Morgantown use :pRT frequently in their tweets for a reason normal humans can not comprehend.
-    # Solution:  Ensure that the locations of tweets are in Morgantown or West Virginia.  This change filtered only the annoying :pRT messages and left the sensible messages alone.
-    if "West Virginia" in tweet[u'location'] or "Morgantown" in tweet[u'location']:
-        print tweet[u'text'];
-    else:
-        continue
+    # Parse the JSON data and print it to the terminal
+    # That's all for now...
+    nativeData = json.loads(data)
+
+    for tweet in nativeData[u'results']:
+        # Problem: Some people that are far, far away from Morgantown use :pRT frequently in their tweets for a reason normal humans can not comprehend.
+        # Solution:  Ensure that the locations of tweets are in Morgantown or West Virginia.  This change filtered only the annoying :pRT messages and left the sensible messages alone.
+        if "West Virginia" in tweet[u'location'] or "Morgantown" in tweet[u'location']:
+            print tweet[u'text'];
+        else:
+            continue
+
+
+if __name__ == "__main__":
+    main()
