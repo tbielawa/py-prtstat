aa75606a61dffc9144c34452c0a5c26ea5bb736f
Ricky Hussmann <ricky.hussmann@gmail.com>
2010-12-14 12:11:20 -0500

    Converted from using argparse to optparse for better backward compatibility to older versions of Python (a.k.a. Mac users). Also, fixed an issue where strings and int's being concatenated was throwing an error.

--- prtstat.py
+++ prtstat.py
@@ -8,7 +8,7 @@ import urllib2;
 import json;
 
 # For selecting what analysis method you want.
-import argparse
+from optparse import OptionParser
 
 def main():
 
@@ -31,13 +31,13 @@ def main():
 
 def ParseArguments():
     # Check sys.argv for arguments.
-    parser = argparse.ArgumentParser(description='A Python script to provide methods to determine if the PRT is running.  Cookies to he who creates the best method.  So say we all.')
-    parser.add_argument('--printtweets',dest='PrintTweets', action='store_true',help='Print the information passed to the classifiers.  Useful if you want to know what\'s going into your classifier!')
-    parser.add_argument('--printtweettext',dest='PrintTweetText', action='store_true',help='Print the plain text of the tweets returned by the Twitter query.')
-    parser.add_argument('--kelsbagofwords',dest='KelsBagOWords', action='store_true',help='Kel\'s simple attempt at using a simple bag of words technique. Intended to inspire others to join in rather than giving any kind of useful data.')
+    parser = OptionParser()
+    parser.add_option('--printtweets',dest='PrintTweets', action='store_true',help='Print the information passed to the classifiers.  Useful if you want to know what\'s going into your classifier!')
+    parser.add_option('--printtweettext',dest='PrintTweetText', action='store_true',help='Print the plain text of the tweets returned by the Twitter query.')
+    parser.add_option('--kelsbagofwords',dest='KelsBagOWords', action='store_true',help='Kel\'s simple attempt at using a simple bag of words technique. Intended to inspire others to join in rather than giving any kind of useful data.')
 
-    args = parser.parse_args()
-    return args
+    (options, args) = parser.parse_args()
+    return options
 
 def PrintTweets(data):
     print data
@@ -53,7 +53,7 @@ def PrintTweetText(data):
 # I can promise before even writing that method will be the suck.
 # Just throwing this in to get the ball rolling :)  I have a better idea I'll throw in later if someone else gives it a go.
 def KelsBagOWords(data):
-    # The idea behind a bag of words technique is that we simply look to see if word x occurs in a tweet.  If more positive words than negative, the PRT is down.
+    # The idea behind a bag of words technique is that we simply look to see if word x occurs in a tweet.  If more negative words than positive, the PRT is down.
     # We don't consider temporal effects ( tweets that are older than a certain age shouldn't be considered)
     # or weighing some users more heavily than others (the official WVU prt twitter account over others).
     GoodWords = ['currently running', 'normal']
@@ -82,9 +82,9 @@ def KelsBagOWords(data):
 
     print "Kel's Bag O' Words method thinks..."
     if Balance > 0:
-        print "The PRT is probably running: "+abs(Balance)/len(data[u'results'])
+        print "The PRT is probably running: " + str(abs(Balance)/len(data[u'results']))
     elif Balance < 0:
-        print "The PRT is probably not running: "+abs(Balance)/len(data[u'results'])
+        print "The PRT is probably not running: " + str(abs(Balance)/len(data[u'results']))
     else:
         print "that you should probably just go look for yourself... No one on Twitter seems to know..."
 
