a62a961e89f796808fb08aeb6f9451526d8f0811
Ricky Hussmann <ricky.hussmann@gmail.com>
2010-12-16 14:34:34 -0500

    Fixed Kel's Bag O Words summary printing.
    After my change to using optparse, Kel's Bag O
    Words summaries were always printing with a
    percetnage of 0%. This was because I was dividing
    an int by and int, with the top always less than
    the bottom (whoops). Changed to a floating point.
    
    Also, a directory for collecting sample data
    from Twitter. Included a sample during a PRT
    outage on December 16th, 2010.

--- prtstat.py
+++ prtstat.py
@@ -95,10 +95,12 @@ def KelsBagOWords(data):
             continue
 
     print "Kel's Bag O' Words method thinks..."
+    result = float(abs(Balance))/float(len(data[u'results']))
+
     if Balance > 0:
-        print "The PRT is probably running: " + str(abs(Balance)/len(data[u'results']))
+        print "The PRT is probably running: " + str(result)
     elif Balance < 0:
-        print "The PRT is probably not running: " + str(abs(Balance)/len(data[u'results']))
+        print "The PRT is probably not running: " + str(result)
     else:
         print "that you should probably just go look for yourself... No one on Twitter seems to know..."
 
