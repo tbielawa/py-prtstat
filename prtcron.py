d9ef0a5a390c003c92d4640512f87247a71796b0
Andrew Butcher <abutcher@redhat.com>
2013-02-13 01:04:52 -0500

    Slim that up.

--- prtcron.py
+++ prtcron.py
@@ -25,67 +25,24 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import urllib2;
-import json;
-import time;
-import datetime;
-import os;
+import json
 import pprint
+from pymongo import Connection
+import urllib2
 
 def main():
-   # Perform the search GET
-    nativeTweetData = TweetData()
-    weatherData = WeatherData()
+    db = client()
+    for tweet in tweet_data():
+        if db['tweets'].find({'id': tweet['id']}).count() == 0:
+            db['tweets'].insert(tweet)
 
-    for tweet in nativeTweetData[u'results']:
+def client():
+    return Connection('173.255.237.153')['prtdata']
 
-        tweetDate = datetime.datetime(*time.strptime(tweet[u'created_at'], "%a, %d %b %Y %H:%M:%S +0000")[0:6])
-        delta = datetime.timedelta(minutes=30)
-        now = datetime.datetime(*time.localtime()[0:6])
-
-        if tweetDate > now - delta:
-            opts = []
-            opts.append('datum[created_at]=\"%s\"' % tweet[u'created_at'])
-            opts.append('datum[from_user]=\"%s\"' % tweet[u'from_user'])
-            opts.append('datum[from_user_name]=\"%s\"' % tweet[u'from_user_name'])
-            opts.append('datum[tweet_id]=\"%s\"' % tweet[u'id'])
-            opts.append('datum[profile_image_url]=\"%s\"' % tweet[u'profile_image_url'])
-            opts.append('datum[source]=\"%s\"' % tweet[u'source'])
-            opts.append('datum[text]=\"%s\"' % tweet[u'text'])    
-            opts.append('datum[humidity]=\"%s\"' % weatherData[u'atmosphere'][u'humidity'])
-            opts.append('datum[pressure]=\"%s\"' % weatherData[u'atmosphere'][u'pressure'])
-            opts.append('datum[visibility]=\"%s\"' % weatherData[u'atmosphere'][u'visibility'])
-            opts.append('datum[temperature]=\"%s\"' % weatherData[u'condition'][u'temperature'])
-            opts.append('datum[wind_direction]=\"%s\"' % weatherData[u'wind'][u'direction'])
-            opts.append('datum[wind_speed]=\"%s\"' % weatherData[u'wind'][u'speed'])
-            opts.append('datum[condition]=\"%s\"' % weatherData[u'condition'][u'text'])
-
-            command = "curl "
-            for opt in opts:
-                command += "-d " + opt + " "
-            command += "prtdata.afrolegs.com/data"
-            os.system(command)
-
-def PrintTweets(data):
-    print data
-
-def PrintTweetText(data):
-    # This prints everything returned by the query.
-    # If you're interested in locality, check out how I filter in KelsBagOWords(data)
-    for Tweet in data[u'results']:
-        print Tweet[u'from_user'], Tweet[u'text']
-
-def TweetData():
+def tweet_data():
     tweet_response = urllib2.urlopen('http://search.twitter.com/search.json?q=WVUDOT&from_user_id_str=85356593')
-    tweet_data = tweet_response.read()
-    nativeTweetData = json.loads(tweet_data)
-    return nativeTweetData
-
-def WeatherData():
-    weather_response = urllib2.urlopen('http://weather.yahooapis.com/forecastjson?w=2454113')
-    weather_data = weather_response.read()
-    nativeWeatherData = json.loads(weather_data)
-    return nativeWeatherData
+    tweet_data = json.loads(tweet_response.read())['results']
+    return tweet_data
 
 if __name__ == "__main__":
     main()
